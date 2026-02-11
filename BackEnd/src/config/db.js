3d3789d04b8c7d1e5c0602ddf48bf442b08b9662
venkynani1 <145059791+venkynani1@users.noreply.github.com>
2026-02-11 10:26:20 +0530

    Update MongoDB connection success message

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI)
-    console.log("MongoDB connected")
+    console.log("MongoDB connected Successfullyy!!!")
   } catch (error) {
   console.error("MongoDB connection failed:", error.message)
   process.exit(1)
